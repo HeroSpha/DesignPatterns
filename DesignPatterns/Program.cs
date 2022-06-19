1f7eb39b6d6b2c25faacd03badb512dc75bdc8de
Siphamandla Ngwenya <ngwenyaspa@gmail.com>
2022-06-19 04:16:37 +0200

    started design pattern in different languages

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,35 @@
+﻿//  implementation
+
+//Singleton pattern
+
+using System.Runtime.CompilerServices;
+
+var fruit = FruitSingleton.Instance;
+fruit.WriteAsync();
+
+
+// Singleton pattern
+
+public class FruitSingleton
+{
+    private static FruitSingleton instance = new();
+    private FruitSingleton(){}
+
+    public static FruitSingleton Instance => instance;
+
+    public void WriteAsync()
+    {
+        Console.WriteLine("Single Instance");
+    }
+}
+
+// prototype pattern 
+
+public class FruitProtoType
+{
+    private static FruitProtoType  instance = new FruitProtoType();
+    
+    private FruitProtoType() {}
+
+    public static FruitProtoType Clone => (FruitProtoType) instance.MemberwiseClone();
+}
